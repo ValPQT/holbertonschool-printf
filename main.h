4e038e24e8b8cf8803c986b7a42a8714a0953c8c
ValPQT <valentinpasquiet@gmail.com>
2025-11-25 13:06:07 +0000

    feat main.h: <created and added main.h>

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * function _printf
+ */
+int _printf(const char *format, ...);
+
+#endif
