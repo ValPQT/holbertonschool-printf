06796f8f59f2749a2bf036d7da4630f4025d1fb4
starfruitone <duranta.morgan@gmail.com>
2025-11-26 14:44:00 +0000

    fix: fixed all compilation issues

--- _printf.c
+++ _printf.c
@@ -4,9 +4,7 @@
 
 int _printf(const char *format, ...)
 {
-	int n;
 	char *str;
-	int i;
 
 	va_list list;
 
@@ -14,18 +12,15 @@ int _printf(const char *format, ...)
 
 	if (format)
 	{
-		for (i = 0; i < n; i++)
-		{
-			str = va_arg(list, char*);
-
-			if (str == NULL)
-				return (-1);
-			else
-				_printf("%s, va_arg(list, char*))
-		}
+		str = va_arg(list, char*);
+		if (str == NULL)
+			return (-1);
+		else
+			printf("%s", str);
 
 		printf("\n");
 
 		va_end(list);
 	}
+	return 0;
 }
