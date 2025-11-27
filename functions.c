4d5e6a677955700a12e6874a94147b38cfd5a727
ValPQT <valentinpasquiet@gmail.com>
2025-11-27 13:30:23 +0000

    feat functions.c : <create and added the file named functions.c>

---
+++ functions.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int print_c(va _list args)
+{
+	int c;
+
+	c = va_arg(arg, int);
+	return (_putchar(c));
+}
+
+/**
+ *
+ *
+ *
+ */
+
+int print_s(va_list args)
+{
+	int i, count = 0;
+	char *str;
+
+	i = 0;
+	str = va_arg(agrs, char*);
+	if (str == NULL)
+		str = "(null)";
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+		count++;
+	}
+	return (count);
+}
+
+/*
+ *
+ *
+ *
+ */
+
+int print_percent(va_list args)
+{
+	char *str;
+
+	str = "%";
+	if (va_arg(args, int) == *str)
+	{
+		return(*str);
+	}
+	return(*str);
+}
