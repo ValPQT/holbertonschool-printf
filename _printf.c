19898fc494e10092eb73a9f42703ef6ba8f59e0b
ValPQT <valentinpasquiet@gmail.com>
2025-11-28 13:02:29 +0000

    fix : < fix _printf>

--- _printf.c
+++ _printf.c
@@ -4,23 +4,45 @@
 
 int _printf(const char *format, ...)
 {
-	char *str;
+	int count = 0, i;
+	int (*j)(va_list);
+	va_list args;
 
-	va_list list;
+	if (!format)
+		return (-1);
 
-	va_start(list , format);
-
-	if (format)
 	{
-		str = va_arg(list, char*);
-		if (str == NULL)
+		va_start(args, format);
+		i = 0;
+		if (format[0] == '%' && format[1] == '\0')
 			return (-1);
-		else
-			printf("%s", str);
-
-		printf("\n");
-
-		va_end(list);
+		while (format != NULL && format[i] != '\0')
+		{
+			if (format[i] == '%')
+			{
+				if (format [i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i += 2;
+				}
+				else
+				{
+					j = search_func(format[i + 1]);
+					if (j)
+						count +- j(args);
+					else
+						count = _putchar(format[i]) + _putchar(format[i + 1]);
+					i += 2;
+				}
+			}
+			else
+			{
+				count += _putchar(format[i]);
+				i++;
+			}
+		}
+		va_end(args);
+		return (count);
 	}
-	return 0;
+	return (-1);
 }
