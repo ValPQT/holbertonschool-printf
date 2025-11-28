98760eec5b26947d42f85e4daed0557700e8a646
ValPQT <valentinpasquiet@gmail.com>
2025-11-28 16:42:59 +0000

    feat : <created and added the function for specifier d>

--- functions.c
+++ functions.c
@@ -57,3 +57,40 @@ int print_percent(va_list args)
 	}
 	return(*str);
 }
+
+/**
+ *
+ *
+ *
+ */
+
+int print_d(va_list args)
+{
+
+	unsigned int absolute, aux, countnum, count;
+	int n;
+
+	count = 0;
+	n = va_arg(args, int);
+		if (n < 0)
+		{
+			absolute = (n * -1);
+			count += _putchar('-');
+		}
+		else
+			absolute = n;
+
+	aux = absolute;
+	countnum = 1;
+	while (aux > 9)
+	{
+		aux /= 10;
+		countnum *= 10;
+	}
+	while (countnum >= 1)
+	{
+		count += _putchar(((absolute / countnum) % 10) + '0');
+		countnum /= 10;
+	}
+	return (count);
+}
--- main.h
+++ main.h
@@ -8,7 +8,7 @@
 
 /**
  * struct specifier - type of specifier in input
- * @valid: the valid character 
+ * @valid: the valid character
  * @f : the functions associated
  */
 
@@ -23,6 +23,7 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list args);
 int print_s(va_list args);
+int print_d(va_list args);
 int print_percent(va_list args);
 int (*search_func(char x))(va_list);
 
